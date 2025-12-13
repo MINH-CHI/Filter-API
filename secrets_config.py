d32cbea53cd9577981ab36cb26d47517526baea6
Minh <minhchitran12345678910@gmail.com>
2025-12-13 20:04:51 +0700

    Tạo mới API key

--- secrets_config.py
+++ secrets_config.py
@@ -1,7 +1,7 @@
 API_KEYS = {
-    "Data_team_zhVz3H6k94kPdlz7bHGEuJzUf7qC2x55": "Sếp khánh",
-    "Data_team_m44RBmGqhV0VYA12fF3mO4k1Yy2fbA9D": "Anh Khôi",
-    "AI_team_6AhG8m0wAZlxODzADZ2iEzAS9Ljc9oRp": "Vương",
-    "AI_team_HYdempDSrAM2hCCr9vdGjHcrPo0Odam4": "Mạnh",
-    "Data_team_91WUA9sKn6qlukP9kw6qRQzJ6mmzAsD5": "Minh",
+    "Data_team_EL6HAA9sQdgN2X8M91SOI2HoqQYItfG1": "Sếp khánh",
+    "Data_team_SKJaHHsEqbfGcEnwwb8XNntmPsOeyebk": "Anh Khôi",
+    "AI_team_HvE2LBvaTMJThMVR31ylyz6q9xipxko3": "Vương",
+    "AI_team_vO11abKWoGMm6U9ZoIg4pOnQdhmOwA01": "Mạnh",
+    "Data_team_Key15hC0V13AmuxoomfpB2YQHe2YG5v0": "Minh",
 }
