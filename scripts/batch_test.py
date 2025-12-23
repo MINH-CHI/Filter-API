7f817388fdaa3f2d90df4ebe975a58328583a10f
Minh <minhchitran12345678910@gmail.com>
2025-12-23 09:10:04 +0700

    Xóa bớt biến token

--- scripts/batch_test.py
+++ scripts/batch_test.py
@@ -28,7 +28,6 @@ API_KEY = os.getenv("API_KEY")
 DRIVE_BASE_FOLDER_NAME = "DATA"
 DRIVE_SUB_FOLDER_NAME = "object_detection"
 DRIVE_VPP_FOLDER_NAME = "classes-do-gia-dung"
-TOKEN_FILE = 'token.json'
 MONGO_URI = os.getenv("MONGO_URI")
 DB_NAME = "api_request_log" 
 COLLECTION_NAME = "api_unlabeled_images"
